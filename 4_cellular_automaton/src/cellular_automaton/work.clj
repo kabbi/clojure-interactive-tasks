2cb325e15f2676c2039b607bd7a44394ec8c9906
kabbi <userdima@gmail.com>
2012-10-23 22:46:10 +0300

    Implemented Wireworld at last, fixed some colors

--- 4_cellular_automaton/src/cellular_automaton/work.clj
+++ 4_cellular_automaton/src/cellular_automaton/work.clj
@@ -58,7 +58,7 @@
 (defn gol-switch-cell [old-state]
 	(if old-state nil :on))
 
-(start-simulation
+#_(start-simulation
 	game-of-life gol-cell-colors gol-switch-cell)
 
 ;;; Brian's Brain (http://en.wikipedia.org/wiki/Brian%27s_Brain)
@@ -84,7 +84,9 @@
 
 (defn brian-brain
 	([]
-		{})
+		; The beautiful pulsar
+		{[34 29] :dying, [33 31] :dying, [32 28] :dying, [31 30] :dying,
+		 [33 29] :on, [33 30] :on, [32 30] :on, [32 29] :on})
 	([prev-state]
 		(-> prev-state bb-filter-dead bb-step)))
 (def bb-cell-colors
@@ -132,37 +134,54 @@
 
 ;;; Wireworld (http://en.wikipedia.org/wiki/Wireworld)
 ; NOT YET IMPLEMENTED
-#_(
+
 (defn ww-filter-cells [cells state]
 	(reduce #(apply assoc % %2) {} (filter #(= (val %) state) cells)))
 (defn ww-filter-empty [cells]
 	(reduce #(apply assoc % %2) {} (filter val cells)))
 
-(defn ww-neighbours [[[x y] _]]
+(defn ww-neighbours [[x y]]
 	(for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)]
 		[(+ dx x) (+ dy y)]))
 
-(defn ww-substep [cells dead]
-	(zipmap (for [[loc n] (frequencies (mapcat ww-neighbours cells))
-			   :when (and (= n 2) (not (dead loc)))]
-			loc) (repeat :on)))
-;(defn ww-step [cells]
-;	(let [dying (ww-filter-cells cells :on)
-;		  alive (ww-substep dying (ww-filter-cells cells :dying))
-;		  dying (zipmap (keys dying) (repeat :dying))]
-;		(merge alive dying)))
+(defn ww-check-signal [cells cell]
+	(let [neighbours (keep cells (ww-neighbours cell))
+		  neis-count (count (filter #{:head} neighbours))]
+		(if (or (= neis-count 1) (= neis-count 2))
+			:head
+			:wire)))
+
 (defn ww-step [cells]
-	(zipmap (for [[[loc state] n] (frequencies (mapcat ww-neighbours cells))
-			   :when (and (= n 2))]
-			loc) (repeat :on)))
+	(into {} (for [[loc type] cells]
+		(cond
+			(= type :head)
+				[loc :tail]
+			(= type :tail)
+				[loc :wire]
+			(= type :wire)
+				[loc (ww-check-signal cells loc)]))))
 
 (defn wire-world
 	([]
-		{})
+		; I've stolen it from Wikipedia
+		{[28 27] :wire, [30 29] :wire, [55 23] :wire, [27 27] :head, [29 29] :wire, [54 23] :wire,
+		 [26 27] :tail, [28 29] :wire, [53 23] :wire, [55 25] :wire, [27 29] :wire, [52 23] :wire,
+		 [54 25] :wire, [25 28] :wire, [26 29] :wire, [53 25] :wire, [55 27] :wire, [51 24] :wire,
+		 [52 25] :wire, [54 27] :wire, [53 27] :wire, [50 25] :wire, [51 26] :wire, [52 27] :wire,
+		 [49 25] :wire, [48 25] :wire, [47 25] :wire, [46 25] :wire, [45 25] :head, [44 25] :tail,
+		 [43 25] :wire, [41 24] :wire, [42 25] :wire, [38 22] :wire, [39 23] :tail, [40 24] :head,
+		 [41 25] :wire, [37 22] :wire, [39 24] :head, [41 26] :wire, [36 22] :wire, [38 24] :head,
+		 [40 26] :wire, [35 22] :wire, [38 25] :wire, [39 26] :wire, [38 26] :wire, [39 27] :wire,
+		 [33 21] :head, [34 22] :wire, [32 21] :tail, [38 28] :wire, [31 21] :wire, [33 23] :wire,
+		 [37 28] :wire, [30 21] :wire, [32 23] :wire, [36 28] :wire, [29 21] :wire, [31 23] :wire,
+		 [35 28] :wire, [28 21] :wire, [30 23] :wire, [27 21] :wire, [29 23] :tail, [33 27] :head,
+		 [34 28] :wire, [26 21] :wire, [28 23] :head, [32 27] :tail, [27 23] :wire, [31 27] :wire,
+		 [33 29] :wire, [25 22] :wire, [26 23] :wire, [30 27] :wire, [32 29] :wire, [29 27] :wire,
+		 [31 29] :wire})
 	([prev-state]
 		(-> prev-state ww-filter-empty ww-step)))
 (def ww-cell-colors
-	{nil '(0), :wire '(142 142 0), :head '(0 142 142), :tail '(142 0 0)})
+	{nil '(0), :wire '(246 214 19), :head '(63 128 255), :tail '(223 73 10)})
 (defn ww-switch-cell [old-state]
 	(cond
 		(= old-state nil) :wire
@@ -170,8 +189,7 @@
 		(= old-state :head) :tail
 		(= old-state :tail) nil))
 
-#_(start-simulation
-	brian-brain ww-cell-colors ww-switch-cell)
-)
+(start-simulation
+	wire-world ww-cell-colors ww-switch-cell)
 
 
