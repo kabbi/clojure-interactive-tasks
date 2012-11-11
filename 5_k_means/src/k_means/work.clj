6430bb4ea030e3b44d6ae7657db43554a293d295
kabbi <userdima@gmail.com>
2012-11-11 18:49:56 +0300

    Almost implemented base algorithm

--- 5_k_means/src/k_means/work.clj
+++ 5_k_means/src/k_means/work.clj
@@ -1,6 +1,8 @@
 (ns k-means.work
   (:use [k-means.core :only [run-empty run-2-circles run-3-circles run-random-circles]]))
 
+(def K (atom 3))
+
 
 ;;; Your task is to implement clustering algorithm.
 ;;; You're a given a set of points on plane. And your goal is to divide them to k clusters.
@@ -22,6 +24,45 @@
 ;;;               space - reset simulation (remove all points or regenerate them, depenends on test)
 ;;; Note that may need use different solutions (with k = 2 for run-2-circles and  k = 3 for run-3-circles).
 
+(defn sqr [x]
+	(* x x))
+
+(defn dist-sqr [[x1 y1] [x2 y2]]
+	(+ (sqr (- x1 x2)) (sqr (- y1 y2))))
+
+(defn choose-starting-clasters [points]
+	(take @K (shuffle points)))
+
+(defn which-cluster [clusters point]
+	(let [dist-map (zipmap (map #(dist-sqr % point) clusters) clusters)]
+		(dist-map (apply min (keys dist-map)))))
+
+(defn average [data]
+	(/ (apply + data) (count data)))
+
+(defn map-points-to-clusters [clusters points]
+	(reduce #(merge-with conj % {(which-cluster clusters %2) %2})
+		(zipmap clusters (repeat []))
+		points))
+
+(defn calc-center [points]
+	[(average (map first points))
+	 (average (map second points))])
+
+(defn move-clusters [clusters points]
+	(reduce #(assoc % (calc-center (val %2)) (val %2)) {} (map-points-to-clusters clusters points)))
+
+;;; Currently this is not really finished :)
+(defn process-clusters [points]
+	(let [clusters (atom (move-clusters (choose-starting-clasters points) points))]
+		(dotimes [_ 10] (swap! clusters #(move-clusters (keys %) points)))
+		@clusters))
+
+(defn k-means [points]
+	(vec (vals (process-clusters points))))
+
+;(run-empty k-means)
+(run-3-circles k-means)
 
 
 ;;; Now try to improve your solution so it can determine k based on given points. So if there are visually 3 clusters it should partition points to 3 clusters, if 4 than to 4 clusters.
