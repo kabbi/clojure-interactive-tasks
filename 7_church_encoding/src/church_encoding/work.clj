2b47d622eb8cf274e3a8246cf5bdb6694aaf2d73
kabbi <userdima@gmail.com>
2012-11-24 17:57:47 +0300

    Implemented some lambda-calculus solutions

--- 7_church_encoding/src/church_encoding/work.clj
+++ 7_church_encoding/src/church_encoding/work.clj
@@ -10,6 +10,8 @@
 
 ;;; Example:
 
+(def church-0 (to-church-num 0))    ; 0 in church numerals
+
 (def church-5 (to-church-num 5))    ; 5 in church numerals
 
 (defn print-star [x] (print "*") x) ; Takes 1 argument, prints a star and retuns argument without modification.
@@ -24,90 +26,103 @@
 
 ;;; Implement + (plus) for church numerals.
 
-(def plus :YOUR_IMPLEMENTATION_HERE)
+(def plus (fn [a] (fn [b] (fn [f] (fn [x]
+	((a f) ((b f) x)))))))
 
 (to-normal-num ((plus church-2) church-2)) ; must return 4
 
 (test-plus plus) ; test your solution
-
+(println)
 
 
 ;;; Implement * (multiplication) for church numerals
 
-(def mult :YOUR_IMPLEMENTATION_HERE)
+(def mult (fn [a] (fn [b] (fn [f] (fn [x]
+	((a (b f)) x))))))
 
 (to-normal-num ((mult church-2) church-5)) ; must return 10
 
 (test-mult mult) ; test your solution
-
+(println)
 
 
 ;;; Implement ^ (pow function) for church numerals.
 
-(def pow :YOUR_IMPLEMENTATION_HERE)
+(def pow (fn [a] (fn [b] (b a))))
 
 (to-normal-num ((pow church-2) church-5)) ; must return 32
 
 (test-pow pow) ; test your solution
-
+(println)
 
 
 ;;; Implement dec function for church numerals.
 
-(def dec :YOUR_IMPLEMENTATION_HERE)
+(def pair (fn [a] (fn [b] (fn [z]
+	((z a) b)))))
+(def cfirst (fn [a] (fn [b] a)))
+(def csecond (fn [a] (fn [b] b)))
+(def cinc (fn [a] (fn [f] (fn [x]
+	((a f) (f x))))))
+(def next-pair (fn [p]
+	((pair (cinc (p cfirst))) (p cfirst))))
 
-(to-normal-num (dec church-5)) ; must return 4
+(def cdec (fn [n]
+	(((n next-pair) ((pair church-0) church-0)) csecond)))
 
-(test-dec dec) ; test your solution
+(to-normal-num (cdec church-5)) ; must return 4
 
-
-
-;;; Implement sum function. sum takes number n and returns sum of all numbers less or equals to n.
-;;; You'll need to use recursion here. For recursion you'll need lazy values.
-;;; You can use delay for that: http://clojuredocs.org/clojure_core/1.2.0/clojure.core/delay
-
-(def sum :YOUR_IMPLEMENTATION_HERE)
-
-(to-normal-num (sum church-2)) ; must return 3
-
-(test-sum sum)
+(test-dec cdec) ; test your solution
 
 
 
-;;; Additional task.
-;;; Implement set of function to create/manipulate lists.
-;;; Your need to implement following functions:
-;;; empty? - checks if list is empty, returns true or false. see church booleans http://en.wikipedia.org/wiki/Church_encoding#Church_booleans
-;;; empty-list - used as "end" of the list.
-;;; head - returns head of a list
-;;; tail - returns tail of a list
-;;; cons - takes 2 arguments h and t, and creates a list such that (head (cons a b)) = a, (tail (cons a b)) = b
-;;;
-;;; Help: http://en.wikipedia.org/wiki/Church_encoding#List_encodings
-
-(def empty? :YOUR_IMPLEMENTATION_HERE)
-
-(def empty-list :YOUR_IMPLEMENTATION_HERE)
-
-(def head :YOUR_IMPLEMENTATION_HERE)
-
-(def tail :YOUR_IMPLEMENTATION_HERE)
-
-(def cons :YOUR_IMPLEMENTATION_HERE)
-
-(((empty? empty-list) true) false) ; must return true
-
-(head (cons "Hello" empty-list)) ; must return "Hello"
-
-(let [list (cons "Hello" empty-list)
-      t (tail list)]
-  ((empty? t) true) false) ; must return true
-
-(test-list {:empty? empty?
-            :empty-list empty-list
-            :head head
-            :tail tail
-            :cons cons}) ; test your solution
-
-
-
+;;;; Implement sum function. sum takes number n and returns sum of all numbers less or equals to n.
+;;;; You'll need to use recursion here. For recursion you'll need lazy values.
+;;;; You can use delay for that: http://clojuredocs.org/clojure_core/1.2.0/clojure.core/delay
+;
+;(def sum :YOUR_IMPLEMENTATION_HERE)
+;
+;(to-normal-num (sum church-2)) ; must return 3
+;
+;(test-sum sum)
+;
+;
+;
+;;;; Additional task.
+;;;; Implement set of function to create/manipulate lists.
+;;;; Your need to implement following functions:
+;;;; empty? - checks if list is empty, returns true or false. see church booleans http://en.wikipedia.org/wiki/Church_encoding#Church_booleans
+;;;; empty-list - used as "end" of the list.
+;;;; head - returns head of a list
+;;;; tail - returns tail of a list
+;;;; cons - takes 2 arguments h and t, and creates a list such that (head (cons a b)) = a, (tail (cons a b)) = b
+;;;;
+;;;; Help: http://en.wikipedia.org/wiki/Church_encoding#List_encodings
+;
+;(def empty? :YOUR_IMPLEMENTATION_HERE)
+;
+;(def empty-list :YOUR_IMPLEMENTATION_HERE)
+;
+;(def head :YOUR_IMPLEMENTATION_HERE)
+;
+;(def tail :YOUR_IMPLEMENTATION_HERE)
+;
+;(def cons :YOUR_IMPLEMENTATION_HERE)
+;
+;(((empty? empty-list) true) false) ; must return true
+;
+;(head (cons "Hello" empty-list)) ; must return "Hello"
+;
+;(let [list (cons "Hello" empty-list)
+;      t (tail list)]
+;  ((empty? t) true) false) ; must return true
+;
+;(test-list {:empty? empty?
+;            :empty-list empty-list
+;            :head head
+;            :tail tail
+;            :cons cons}) ; test your solution
+;
+;
+;
+;
